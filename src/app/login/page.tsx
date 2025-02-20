53495ee362c643ede07691599ccc01ad37f542d4
hiroto <kitta1213@gmail.com>
2025-02-20 13:11:10 +0900

    【修正_UI実装】ログイン画面

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,50 +3,42 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import { Trees } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 
 export default function LoginPage() {
   return (
-    <div className="container flex h-screen w-screen flex-col items-center justify-center">
-      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-        <div className="flex flex-col space-y-2 text-center">
-          <div className="flex items-center justify-center gap-2">
-            <Trees className="h-8 w-8 text-primary" />
-            <h1 className="text-2xl font-bold text-primary">FAMILY PARK SHARE</h1>
-          </div>
-        </div>
-
-        <Card>
-          <CardHeader>
+    <div className="flex h-[calc(100vh-4rem)] w-full flex-col items-center justify-start pt-12">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[450px]">
+        <Card className="border-2">
+          <CardHeader className="space-y-1">
             <CardTitle className="text-center text-2xl">ログイン</CardTitle>
           </CardHeader>
           <CardContent>
-            <div className="space-y-4">
-              <div className="space-y-2">
-                <label className="text-sm font-medium leading-none" htmlFor="email">
+            <div className="space-y-6">
+              <div>
+                <label className="text-sm leading-none font-bold" htmlFor="email">
                   メールアドレス
                 </label>
                 <Input
                   id="email"
-                  placeholder="tarou.yamada@example.com"
                   type="email"
+                  className="h-12 text-lg"
                   autoCapitalize="none"
                   autoComplete="email"
                   autoCorrect="off"
                 />
               </div>
-              <div className="space-y-2">
-                <label className="text-ms font-medium leading-none" htmlFor="password">
+              <div>
+                <label className="text-sm font-bold leading-none" htmlFor="password">
                   パスワード
                 </label>
-                <Input id="password" type="password" />
+                <Input id="password" type="password" className="h-12 text-lg" />
               </div>
-              <Button className="w-full">ログイン</Button>
+              <Button className="w-full h-12 text-lg">ログイン</Button>
             </div>
 
-            <div className="mt-4 text-center text-sm">
+            <div className="mt-6 text-center text-base">
               アカウントをお持ちでない方は
               <Link href="/signin" className="text-primary hover:underline">
                 新規登録
